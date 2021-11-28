4db826efe209aa2f93c7a93e5bc816067cb0b64c
Wilson Sanchez <wrs5779@rit.edu>
2021-11-28 13:16:01 -0500

    HelloVapor File

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,32 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        //http://localhost:8080
+        return "It works great!"
     }
 
     app.get("hello") { req -> String in
-        return "Hello, world!"
+        //http://localhost:8080/hello
+        return "Hello, World!"
+    }
+    
+    app.get("hello", ":name") { req -> String in
+        //http://localhost:8080/hello/tim
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        
+        return "Hello, \(name)!"
+    }
+    
+    app.post("info") { req -> String in
+        let data = try req.content.decode(InfoData.self)
+        return "Hello \(data.name)!"
     }
 }
+
+struct InfoData : Content {
+    let name : String
+}
+
+
